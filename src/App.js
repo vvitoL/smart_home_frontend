0fdcb9c95ff60fbbfd622f00ffc2d45e0aab4b57
Witek <lipnicki.witold@gmail.com>
2022-12-22 16:20:28 +0100

    first schema

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [value, setValue] = useState(0);
+    const [myValue, setMyValue] = useState(null);
+
+    const [devices, setDevices] = useState()
+    // let value = 0;
+    // console.log('running app.js');
+    // console.log('my val', myValue);
+
+    const myFavVal = localStorage.getItem('myFavVal');
+    // console.log('my fav val', myFavVal);
+
+    useEffect(()=>{
+        if (myFavVal) {
+            setMyValue(myFavVal)
+        }
+    }, [value]);
+
+    useEffect(()=>{
+        if (myFavVal) {
+            setMyValue(myFavVal)
+        }
+    }, [value]);
+
+    const handleIncrement = (e) => {
+        setValue(value+1);
+    }
+
+    const handleSaveMyValue = () => {
+        // console.log('saved', value);
+        localStorage.setItem('myFavVal', value)
+        setMyValue(value);
+    };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div> Hello World.! </div>
+          <p>{value}</p>
+          <p>{myValue ?myValue :  '---'}</p>
+          <button onClick={handleIncrement}>Increment</button>
+          <button onClick={handleSaveMyValue}>Save Favourite</button>
       </header>
     </div>
   );
